a4314c44d2ddbd8caf276a2b5d3080b363bf2795
giathanhle <69889127+giathanh890@users.noreply.github.com>
2020-11-29 14:26:05 +0700

    code

---
+++ page 1-nendoroid/js/slideshow.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+showSlides();
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000);
+}
+
+var itemIndex = 0;
+showitem();
+
+function showitem() {
+  var i;
+  var items = document.getElementsByClassName("content1");
+  for (i = 0; i < items.length; i++) {
+    items[i].style.display = "none";  
+  }
+  itemIndex++;
+  if (itemIndex > items.length) {itemIndex = 1}    
+  items[itemIndex-1].style.display = "flex";  
+  setTimeout(showitem, 2500);
+}
+
